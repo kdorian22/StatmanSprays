4d66c8b5375808cc65e4942e1bc29149dd211322
Kenneth Dorian <kenneth.dorian@wustl.edu>
2020-11-26 21:35:54 -0500

    init file correction

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+from app.config import Config
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
