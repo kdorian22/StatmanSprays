317b8cbc3e84326d96d39df6f5a6d34306ddd862
Kenneth Dorian <kenneth.dorian@wustl.edu>
2023-09-23 20:35:46 -0400

    wrap queries in text()

--- statman/views.py
+++ statman/views.py
@@ -1,6 +1,7 @@
 from flask import *
 from flask_jsglue import JSGlue
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 from flask_mobility import Mobility
 from flask_mobility.decorators import mobile_template
 from flask_login import login_required, current_user, login_user, logout_user
@@ -56,14 +57,14 @@ def exists(text):
 @app.route('/')
 def index():
 	with db.engine.connect() as conn:
-		data = conn.execute(f"""SELECT p.TEAM_KEY, NAME, COUNT(*) FROM PLAYER_DIM p
+		data = conn.execute(text(f"""SELECT p.TEAM_KEY, NAME, COUNT(*) FROM PLAYER_DIM p
 		JOIN TEAM_DIM t on t.TEAM_KEY = p.TEAM_KEY
 		WHERE p.ACTIVE_RECORD = 1 and t.ACTIVE_RECORD = 1
-		GROUP BY p.TEAM_KEY;""").fetchall()
+		GROUP BY p.TEAM_KEY;""")).fetchall()
 
-		conn.execute("""SELECT p.*, FULL_NAME FROM PLAY_BY_PLAY p
+		conn.execute(text("""SELECT p.*, FULL_NAME FROM PLAY_BY_PLAY p
 		JOIN PLAYER_DIM d on d.PLAYER_KEY = p.BATTER_PLAYER_KEY
-		WHERE d.ACTIVE_RECORD = 1 AND BATTER_TEAM_KEY = 2 and p.ACTIVE_RECORD = 1 Limit 200""")
+		WHERE d.ACTIVE_RECORD = 1 AND BATTER_TEAM_KEY = 2 and p.ACTIVE_RECORD = 1 Limit 200"""))
 
 	teams = []
 	for d in data:
@@ -126,7 +127,7 @@ def logout():
 def updateTeamDim():
 	update = request.values.get('update', 'u')
 	with db.engine.connect() as conn:
-		teamList = list(conn.execute("""SELECT TEAM_KEY, NAME, ALT_NAMES FROM TEAM_DIM""").fetchall())
+		teamList = list(conn.execute(text("""SELECT TEAM_KEY, NAME, ALT_NAMES FROM TEAM_DIM""")).fetchall())
 	newTeams = scrapeTeams()
 
 	if update == 'd':
@@ -153,11 +154,11 @@ def updateTeamDim():
 			elif curName != newName[0] and newName[0] not in curAltList:
 				curAltList.append(newName[0])
 				with db.engine.connect() as conn:
-					conn.execute(f"""UPDATE TEAM_DIM SET ALT_NAMES = '{', '.join(curAltList)}' WHERE TEAM_KEY = {key}""")
+					conn.execute(text(f"""UPDATE TEAM_DIM SET ALT_NAMES = '{', '.join(curAltList)}' WHERE TEAM_KEY = {key}"""))
 	db.session.commit()
 
 	with db.engine.connect() as conn:
-		data = list(conn.execute('SELECT * FROM TEAM_DIM WHERE ACTIVE_RECORD = 1;').fetchall())
+		data = list(conn.execute(text('SELECT * FROM TEAM_DIM WHERE ACTIVE_RECORD = 1;')).fetchall())
 	return render_template('teamList.html', data = jsonDump(data))
 
 
@@ -178,7 +179,7 @@ def scrapeRoster():
 			db.session.commit()
 
 	with db.engine.connect() as conn:
-		players = list(conn.execute(f"SELECT * FROM PLAYER_DIM WHERE YEAR = '{year}' and TEAM_KEY = {team}")).fetchall()
+		players = list(conn.execute(text(f"SELECT * FROM PLAYER_DIM WHERE YEAR = '{year}' and TEAM_KEY = {team}"))).fetchall()
 	db.session.commit()
 
 	return jsonDump(players)
@@ -665,7 +666,7 @@ def dataDownload():
 	ORDER BY NAME;
 	"""
 	with db.engine.connect() as conn:
-		status = list(conn.execute(query).fetchall())
+		status = list(conn.execute(text(query)).fetchall())
 
 	return render_template('dataDownload.html', status=status, data = jsonDump(status), years=years)
 
@@ -678,10 +679,10 @@ def sprays():
 	if team != '':
 		team = int(team)
 	with db.engine.connect() as conn:
-		data = conn.execute(f"""SELECT p.TEAM_KEY, NAME, COUNT(*) FROM PLAYER_DIM p
+		data = conn.execute(text(f"""SELECT p.TEAM_KEY, NAME, COUNT(*) FROM PLAYER_DIM p
 		JOIN TEAM_DIM t on t.TEAM_KEY = p.TEAM_KEY
 		WHERE p.ACTIVE_RECORD = 1 and t.ACTIVE_RECORD = 1
-		GROUP BY p.TEAM_KEY;""").fetchall()
+		GROUP BY p.TEAM_KEY;""")).fetchall()
 		teams = []
 		for d in data:
 			teams.append({'NAME': d.NAME, 'TEAM_KEY': d.TEAM_KEY})
@@ -693,11 +694,11 @@ def sprays():
 			return render_template('sprays.html', team = team, stats = jsonDump(stats), plays = jsonDump(plays), rosters = jsonDump(rosters), data = jsonDump(teams), years = years)
 
 		num = row.VISITS
-		conn.execute(f"""UPDATE TEAM_DIM SET VISITS = {num+1} WHERE TEAM_KEY = {team}""")
+		conn.execute(text(f"""UPDATE TEAM_DIM SET VISITS = {num+1} WHERE TEAM_KEY = {team}"""))
 
-		plays = list(conn.execute(f"""SELECT * FROM PLAY_BY_PLAY WHERE BATTER_TEAM_KEY = {team} and ACTIVE_RECORD = 1""").fetchall())
-		rosters = list(conn.execute(f"""SELECT * FROM PLAYER_DIM WHERE TEAM_KEY = {team} AND ACTIVE_RECORD = 1 ORDER BY FULL_NAME""").fetchall())
-		stats = list(conn.execute(f"""SELECT * FROM HITTER_STATS WHERE TEAM_KEY = {team} AND ACTIVE_RECORD = 1""").fetchall())
+		plays = list(conn.execute(text(f"""SELECT * FROM PLAY_BY_PLAY WHERE BATTER_TEAM_KEY = {team} and ACTIVE_RECORD = 1""")).fetchall())
+		rosters = list(conn.execute(text(f"""SELECT * FROM PLAYER_DIM WHERE TEAM_KEY = {team} AND ACTIVE_RECORD = 1 ORDER BY FULL_NAME""")).fetchall())
+		stats = list(conn.execute(text(f"""SELECT * FROM HITTER_STATS WHERE TEAM_KEY = {team} AND ACTIVE_RECORD = 1""")).fetchall())
 
 	return render_template('sprays.html', team = team, stats = jsonDump(stats), plays = jsonDump(plays), rosters = jsonDump(rosters), data = json.dumps(teams), years = years)
 
@@ -714,32 +715,32 @@ def printSprays():
 	c = request.values.get('c','')
 	with db.engine.connect() as conn:
 		if c == 'ca':
-			names = list(conn.execute(f"""
+			names = list(conn.execute(text(f"""
 			SELECT * FROM PLAYER_DIM WHERE TEAM_KEY = {team} and YEAR = {year}
-			""").fetchall())
+			""")).fetchall())
 			plays = []
 			stats = []
 			for name in names:
-				plays = plays + list(conn.execute(f"""
+				plays = plays + list(conn.execute(text(f"""
 				SELECT d.FULL_NAME, d.NUMBER, p.* FROM PLAY_BY_PLAY p
 				JOIN PLAYER_DIM d on d.PLAYER_KEY = p.BATTER_PLAYER_KEY
 				WHERE d.FULL_NAME = '{str(name.FULL_NAME).replace("'","''")}' and d.TEAM_KEY = {team} and p.ACTIVE_RECORD = 1
-				""").fetchall())
-				stats = stats + list(conn.execute(f"""
+				""")).fetchall())
+				stats = stats + list(conn.execute(text(f"""
 				SELECT * FROM HITTER_STATS
-				WHERE ACTIVE_RECORD = 1 and FULL_NAME = '{str(name.FULL_NAME).replace("'","''")}' and TEAM_KEY = {team}""").fetchall())
+				WHERE ACTIVE_RECORD = 1 and FULL_NAME = '{str(name.FULL_NAME).replace("'","''")}' and TEAM_KEY = {team}""")).fetchall())
 			return render_template('printSpraysCA.html', names = jsonDump(names), stats = jsonDump(stats), plays = jsonDump(plays))
 
 		if c == 'c':
 			name = player_dim.query.filter_by(PLAYER_KEY=key).first().FULL_NAME
-			plays = list(conn.execute(f"""
+			plays = list(conn.execute(text(f"""
 			SELECT d.FULL_NAME, d.NUMBER, p.* FROM PLAY_BY_PLAY p
 			JOIN PLAYER_DIM d on d.PLAYER_KEY = p.BATTER_PLAYER_KEY
 			WHERE FULL_NAME = '{str(name).replace("'","''")}' and d.TEAM_KEY = {team} and p.ACTIVE_RECORD = 1
-			""").fetchall())
-			stats = list(conn.execute(f"""
+			""")).fetchall())
+			stats = list(conn.execute(text(f"""
 			SELECT * FROM HITTER_STATS
-			WHERE ACTIVE_RECORD = 1 and FULL_NAME = '{str(name).replace("'","''")}' and TEAM_KEY = {team}""").fetchall())
+			WHERE ACTIVE_RECORD = 1 and FULL_NAME = '{str(name).replace("'","''")}' and TEAM_KEY = {team}""")).fetchall())
 			return render_template('printSpraysC.html', name = name, stats = jsonDump(stats), plays = jsonDump(plays))
 
 		if key != '':
@@ -747,25 +748,25 @@ def printSprays():
 			keyList = [keys]
 
 		if key == '':
-			keys = list(conn.execute(f"""SELECT * FROM PLAYER_DIM
-			WHERE TEAM_KEY = {team} and YEAR = {year} and ACTIVE_RECORD = 1""").fetchall())
+			keys = list(conn.execute(text(f"""SELECT * FROM PLAYER_DIM
+			WHERE TEAM_KEY = {team} and YEAR = {year} and ACTIVE_RECORD = 1""")).fetchall())
 			keyList = [str(x.PLAYER_KEY) for x in keys]
 			keys = ', '.join(keyList)
 
 
-		plays = list(conn.execute(f"""
+		plays = list(conn.execute(text(f"""
 		SELECT d.FULL_NAME, d.NUMBER, p.* FROM PLAY_BY_PLAY p
 		JOIN PLAYER_DIM d on d.PLAYER_KEY = p.BATTER_PLAYER_KEY
 		WHERE BATTER_PLAYER_KEY in ({keys}) and p.ACTIVE_RECORD = 1
-		""").fetchall())
+		""")).fetchall())
 
-		stats = list(conn.execute(f"""
+		stats = list(conn.execute(text(f"""
 		SELECT p.PLAYER_KEY, h.* FROM HITTER_STATS h
 		JOIN PLAYER_DIM p on p.FULL_NAME = h.FULL_NAME
 		and h.TEAM_KEY = p.TEAM_KEY and h.NUMBER = p.NUMBER
 		and h.POSITION = p.POSITION and h.CLASS = p.CLASS and h.YEAR = p.YEAR
 		WHERE p.PLAYER_KEY in ({keys})
-		""").fetchall())
+		""")).fetchall())
 
 	return render_template('printSprays.html', keys = keyList, plays = jsonDump(plays), stats = jsonDump(stats))
 
@@ -780,7 +781,7 @@ def faq():
 @app.route('/teamList')
 def teamList():
 	with db.engine.connect() as conn:
-		return render_template('teamList.html', data = jsonDump(list(conn.execute('SELECT * FROM TEAM_DIM WHERE ACTIVE_RECORD = 1;').fetchall())))
+		return render_template('teamList.html', data = jsonDump(list(conn.execute(text('SELECT * FROM TEAM_DIM WHERE ACTIVE_RECORD = 1;')).fetchall())))
 
 @app.route('/admin')
 @login_required
@@ -795,12 +796,12 @@ def editPlays():
 	team_key = int(request.values.get('team', '755'))
 	year = int(request.values.get('year', years[0]))
 	with db.engine.connect() as conn:
-		playsORG = list(conn.execute(f"""SELECT p.FULL_NAME NAME, t.NAME TEAM_NAME, pbp.* FROM PLAY_BY_PLAY pbp
+		playsORG = list(conn.execute(text(f"""SELECT p.FULL_NAME NAME, t.NAME TEAM_NAME, pbp.* FROM PLAY_BY_PLAY pbp
 	 	LEFT JOIN PLAYER_DIM p on p.PLAYER_KEY = pbp.BATTER_PLAYER_KEY
 	 	LEFT JOIN TEAM_DIM t on t.TEAM_KEY = pbp.BATTER_TEAM_KEY
 		WHERE t.TEAM_KEY = {team_key} and pbp.YEAR = {year}
 	 	ORDER BY pbp.DATE_KEY, pbp.BATTER_PLAYER_KEY
-	 	 """).fetchall())
+	 	 """)).fetchall())
 	name = ''
 	if len(playsORG) > 0:
 		name = str(playsORG[0].TEAM_NAME)
@@ -851,7 +852,7 @@ def download(key, year, type, csv):
 		query = f"""SELECT * FROM {tab} WHERE {keyCol} = {key} and YEAR = {year} and ACTIVE_RECORD = 1 ORDER BY {order}"""
 		with db.engine.connect() as conn:
 			if ';' not in query:
-				data = list(conn.execute(query).fetchall())
+				data = list(conn.execute(text(text(query))).fetchall())
 			else:
 				data = []
 	else:
@@ -875,7 +876,7 @@ def getStats(name, num, pos, fresh, year, team):
 	string = f"""SELECT * FROM HITTER_STATS WHERE FULL_NAME = '{name}' and NUMBER = '{num}' and POSITION = '{pos}' and YEAR = '{year}' and CLASS = '{fresh}' and TEAM_KEY = '{team}'"""
 	with db.engine.connect() as conn:
 		if ';' not in string:
-			data = list(conn.execute(string).fetchall())
+			data = list(conn.execute(text(string)).fetchall())
 		else:
 			data = []
 	return jsonDump(data)
